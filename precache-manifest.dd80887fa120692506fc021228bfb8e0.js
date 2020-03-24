04f61819031a9c1322c0dee4faa46c81616dffe8
deepakadhana <deepak.adhana@signcatch.com>
2020-03-24 14:42:37 +0530

    Updates

---
+++ precache-manifest.dd80887fa120692506fc021228bfb8e0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53dbc6f76d3fe23a6a952b256364d14e",
+    "url": "/bookmyshow/index.html"
+  },
+  {
+    "revision": "3629747a19c4c5743a73",
+    "url": "/bookmyshow/static/css/2.34ad86ec.chunk.css"
+  },
+  {
+    "revision": "39a47689986860d40554",
+    "url": "/bookmyshow/static/css/main.244750bc.chunk.css"
+  },
+  {
+    "revision": "3629747a19c4c5743a73",
+    "url": "/bookmyshow/static/js/2.b280bc79.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/bookmyshow/static/js/2.b280bc79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a47689986860d40554",
+    "url": "/bookmyshow/static/js/main.86687f59.chunk.js"
+  },
+  {
+    "revision": "560de84572ab94a909c1",
+    "url": "/bookmyshow/static/js/runtime-main.3a7f34d6.js"
+  },
+  {
+    "revision": "520159d1ca858e2d8a184dd5763dea57",
+    "url": "/bookmyshow/static/media/banner-1.520159d1.jpg"
+  },
+  {
+    "revision": "cfa8bf18ae5b3361589e3152d8a9904e",
+    "url": "/bookmyshow/static/media/banner-2.cfa8bf18.jpg"
+  },
+  {
+    "revision": "e9b339bac9c1956cc128a7ebc4b98f60",
+    "url": "/bookmyshow/static/media/banner-3.e9b339ba.jpg"
+  },
+  {
+    "revision": "9858dffbde73d7c6c023cb8748aba111",
+    "url": "/bookmyshow/static/media/book-my-show-logo-png-6.9858dffb.png"
+  },
+  {
+    "revision": "140c15649bf00c24145616f308491d9a",
+    "url": "/bookmyshow/static/media/movie-poster.140c1564.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/bookmyshow/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/bookmyshow/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/bookmyshow/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/bookmyshow/static/media/slick.f97e3bbf.svg"
+  }
+]);
